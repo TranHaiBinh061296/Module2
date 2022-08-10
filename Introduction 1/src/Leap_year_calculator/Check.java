1654faab03f8cf5f57254705b62c8fc15ba556a3
Hinh Bai <haibinh.061296@gmail.com>
2022-08-10 10:17:31 +0700

    file kiem tra nam nhuan

--- Introduction 1/src/Leap_year_calculator/Check.java
+++ Introduction 1/src/Leap_year_calculator/Check.java
@@ -1,7 +1,13 @@
 package Leap_year_calculator;
 
+import java.util.Scanner;
+
 public class Check {
     public static void main(String[] args) {
+        Scanner ye = new Scanner(System.in);
+        System.out.println("Enter the year ");
+        int year = ye.nextInt();
+
         
     }
 }
