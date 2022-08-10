59e64983a41a75c967ea4496d492f046645f6e96
Hinh Bai <haibinh.061296@gmail.com>
2022-08-10 09:46:15 +0700

    sdfsaf

---
+++ Introduction 1/src/Leap_year_calculator/Check.java
@@ -0,0 +1,7 @@
+package Leap_year_calculator;
+
+public class Check {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Introduction 1/src/Show_the_time/SystemTime.java
@@ -0,0 +1,10 @@
+package Show_the_time;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); //  Lấy ra thời gian hiện tại
+        System.out.println("Now is :" + now);
+    }
+}
