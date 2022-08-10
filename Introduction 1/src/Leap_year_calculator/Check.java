b513a6da0f8a60503cdc3e8cfc2ef3fb4cef2553
Hinh Bai <haibinh.061296@gmail.com>
2022-08-10 11:14:29 +0700

    sdfsadf

--- Introduction 1/src/Leap_year_calculator/Check.java
+++ Introduction 1/src/Leap_year_calculator/Check.java
@@ -7,7 +7,18 @@ public class Check {
         Scanner ye = new Scanner(System.in);
         System.out.println("Enter the year ");
         int year = ye.nextInt();
-
-        
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.printf("%d is leap year", year);
+                } else {
+                    System.out.printf("%d is not leap year", year);
+                }
+            } else {
+                System.out.printf("%d is leap year", year);
+            }
+        } else {
+            System.out.printf("%d is not leap year", year);
+        }
     }
 }
---
+++ Introduction 1/src/Show_greetings/Hello.java
@@ -0,0 +1,10 @@
+package Show_greetings;
+
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner hi = new Scanner(System.in);
+        String text = hi.nextLine("Enter your")
+    }
+}
