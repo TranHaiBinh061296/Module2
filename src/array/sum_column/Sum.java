3f4f395aa4e61980dda42f126440a655dfc9e8e8
Hinh Bai <haibinh.061296@gmail.com>
2022-08-12 17:16:37 +0700

    Tính tổng các đường chéo trong ma trận mảng

--- src/array/sum_column/Sum.java
+++ src/array/sum_column/Sum.java
@@ -16,20 +16,13 @@ public class Sum {
                 arr[i][j] = (int) Math.floor(Math.random() * (10 - 2 + 1) + 2);
                 System.out.print(arr[i][j] + " ");
             }
-            System.out.println();
+            System.out.println("");
         }
+        // tính tổng các số ở đường chéo chính
         int sum = 0;
-        // nhập vào stt 1 cột
-        System.out.println("Enter column :");
-        int number = sc.nextInt();
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (j == number) {
-                    sum += arr[i][j];
-                }
-            }
+                    sum += arr[i][i];
         }
-        // tổng cột đó.
         System.out.print("Sum is: " + sum);
     }
 }
---
+++ src/array/sum_square_matrix/Sum.java
@@ -0,0 +1,23 @@
+package array.sum_square_matrix;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập hàng và cột (trùng nhau) :");
+        int row = sc.nextInt();
+        int column = sc.nextInt();
+        int array [][] = new int[row][column];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) Math.floor(Math.random()*(20-5 + 1) + 5);
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println("");
+        }
+        int sum = 0;
+
+    }
+}
