b38257d8f4f2fa08ec1a0e6561c171e884babaaf
Hinh Bai <haibinh.061296@gmail.com>
2022-08-12 10:00:54 +0700

    Xóa phần tử trong mảng

--- src/array/Reverse_array/main.java
+++ src/array/Reverse_array/main.java
@@ -7,6 +7,7 @@ public class main {
         int size;
         int[] array;
         Scanner input = new Scanner(System.in);
+        //Khai báo các biến, nhập và kiểm tra kích thước mảng
         do {
             System.out.println("Enter size arrays");
             size = input.nextInt();
@@ -14,6 +15,7 @@ public class main {
                 System.out.printf("Size does not exceed 20");
             }
         } while (size > 20);
+        //Nhập giá trị cho các phần tử của mảng
         array = new int[size];
         int i = 0;
         while (i < array.length) {
@@ -21,15 +23,19 @@ public class main {
             array[i] = input.nextInt();
             i++;
         }
+        //In ra mảng đã nhập
         System.out.printf("%-20s%s", "Elements is array : ", "");
         for (int j = 0; j < array.length; j++) {
             System.out.print(array[j] + "\t");
         }
+        //Đảo ngược thứ tự các phần tử trong mảng
+        //Sử dụng biến trung gian để hoán đổi giá trị giữa 2 phần tử.
         for (int j = 0; j < array.length / 2; j++) {
             int temp = array[j];
             array[j] = array[size - 1 - j];
             array[size - 1 - j] = temp;
         }
+        //In ra mảng sau khi đã đảo ngược thứ tự các phần tử
         System.out.printf("\n%-20s%s", "Reverse array : " , "");
         for (int j = 0; j < array.length; j++) {
             System.out.print(array[j] +"\t");
