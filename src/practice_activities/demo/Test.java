cac10a3897cbecb0937633461cecfcf4356122fe
Hinh Bai <haibinh.061296@gmail.com>
2022-08-19 11:39:18 +0700

    Hệ các đối tượng hình học

---
+++ src/practice_activities/demo/Test.java
@@ -0,0 +1,9 @@
+package practice_activities.demo;
+
+public class Test {
+    public static void main(String[] args) {
+        //System.out.println(Circle.PI);
+        //Circle.test();
+        Circle circle = new Circle();
+    }
+}
